78c69fdca1a62356d6c8ceb1394362ab2186cf87
Aryan Gupta <kuantum.freak@gmail.com>
2019-08-21 18:11:39 -0400

    Update with extra credit code

--- p1/gcd.cpp
+++ p1/gcd.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <utility>
 #include <tuple>
+#include <cmath>
 
 // kms... https://stackoverflow.com/questions/19572512
 template <typename T1, typename T2>
@@ -9,38 +10,26 @@ std::pair<T1, T2> mp(T1&& a, T2&& b) {
 	return { a, b };
 }
 
-std::pair<int, bool> perform_euclidean_algo(int a, int b) {
+std::tuple<int, bool, int, int> perform_euclidean_algo(int a, int b) {
 	// Check for the 0 cases
 	if (a == 0 and b == 0) { // a nor b
-		return { 0, false };
-	} else if (a == 0) {
-		return { b, true };
-	} else if (b == 0) {
-		return { a, true };
+		return { 0, false, 0, 0 };
 	}
 
-	// gcd of 5, 5 is 5
-	if (a == b) {
-		return { a, true };
+	int x = 0, o_x = 1,
+	    y = 1, o_y = 0,
+	    r = b, o_r = a;
+	
+	while (r != 0) {
+		int q = std::floor(o_r / r);
+		std::tie(o_r, r) = mp(r, o_r % r);
+		std::tie(o_x, x) = mp(x, o_x - q * x);
+		std::tie(o_y, y) = mp(y, o_y - q * y);
 	}
 
-	// get the difference between the numbers (positive difference)
-	// and reperform eucl algo. This is a bit complecated shorthand
-	// for the non-structural binding version.
-	// python equvilant: na, nb = (a, b - a) if (a < b) else (b, a - b) 
-#ifdef __cpp_structured_bindings
-	auto [na, nb] = (a < b) ? mp(a, b - a) : mp(b, a - b);
-#else
-	int na, nb;
-	if (a < b) {
-		na = a;
-		nb = b - a;
-	} else {
-		na = b;
-		nb = a - b;
-	}
-#endif
-	return perform_euclidean_algo(na, nb);
+	x = o_x % (b / o_r);
+	y = o_y % (-a / o_r);
+	return { o_r, true, x, y };
 }
 
 int main(int ac, char **av) {
@@ -52,13 +41,14 @@ int main(int ac, char **av) {
 	std::cin >> num2;
 
 #ifdef __cpp_structured_bindings
-	auto [gcd, valid] = ::perform_euclidean_algo(num1, num2);
+	auto [gcd, valid, x, y] = ::perform_euclidean_algo(num1, num2);
 #else
-	int gcd; bool valid;
-	std::tie(gcd, valid) = ::perform_euclidean_algo(num1, num2);
+	int gcd; bool valid; int x, y;
+	std::tie(gcd, valid, x, y) = ::perform_euclidean_algo(num1, num2);
 #endif
 	if (valid) {
 		std::cout << "GCD:: " << gcd << std::endl;
+		std::cout << "Bezout's Identity:: " << x << "*" << num1 << " + " << y<< "*" << num2 << " = " << gcd << std::endl;
 	} else {
 		std::cout << "No GCD found" << std::endl;
 	}
