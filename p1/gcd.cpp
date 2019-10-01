d74f0f4a1acce9bfe2d2f3ed9b586988d5d92518
Aryan Gupta <kuantum.freak@gmail.com>
2019-10-01 19:38:10 -0400

    Fix euclidean algo for prof taste

--- p1/gcd.cpp
+++ p1/gcd.cpp
@@ -3,11 +3,19 @@
 #include <utility>
 #include <tuple>
 #include <cmath>
+#include <assert.h>
 
 // kms... https://stackoverflow.com/questions/19572512
 template <typename T1, typename T2>
 std::pair<T1, T2> mp(T1&& a, T2&& b) {
 	return { a, b };
+}
+
+std::pair<int, int> division_algo(int num, int den) {
+	assert(den > 0);
+	int quot = num / den; // will auto round down
+	int rem = num - den * quot;
+	return { quot, rem };
 }
 
 std::tuple<int, bool, int, int> perform_euclidean_algo(int a, int b) {
@@ -16,29 +24,53 @@ std::tuple<int, bool, int, int> perform_euclidean_algo(int a, int b) {
 		return { 0, false, 0, 0 };
 	}
 
-	int x = 0, o_x = 1,
-	    y = 1, o_y = 0,
-	    r = b, o_r = a;
-	
-	while (r != 0) {
-		int q = std::floor(o_r / r);
-		std::tie(o_r, r) = mp(r, o_r % r);
-		std::tie(o_x, x) = mp(x, o_x - q * x);
-		std::tie(o_y, y) = mp(y, o_y - q * y);
+	// Do division algo: a = b * quot + rem
+#ifdef __cpp_structured_bindings
+	auto [quot, rem] = ::division_algo(a, b);
+#else
+	int quot, rem;
+	std::tie(quot, rem) = ::division_algo(a, b);
+#endif
+
+	std::cout << a << " = " << b << " * " << quot << " + " << rem << std::endl;
+
+	// if we found the gcd then return true
+	if (rem == 0) {
+		return { b, true, 0, 0 };
 	}
 
-	x = o_x % (b / o_r);
-	y = o_y % (-a / o_r);
-	return { o_r, true, x, y };
+#ifdef __cpp_structured_bindings
+	auto [gcd, found, u, v] = perform_euclidean_algo(b, rem);
+#else
+	int gcd, u, v;
+	bool found;
+	std::tie(gcd, found, u, v) = perform_euclidean_algo(b, rem);
+#endif
+
+	assert(found == true); // for debugging
+
+	if (u == 0 and v == 0) {
+		return { gcd, found, 1, 1 };
+	}
+
+	int new_v = v * quot + u;
+	u = v;
+	v = new_v;
+
+	return { gcd, true, u, v };
 }
 
 int main(int ac, char **av) {
 	std::cout << "Enter Number 1::";
-	int num1; 
+	int num1;
 	std::cin >> num1;
-	std::cout << "Enter Number 2::";
+	std::cout << "Enter Number 2::" << std::endl;
 	int num2;
 	std::cin >> num2;
+
+	if (num1 < num2) {
+		std::swap(num2, num1);
+	}
 
 #ifdef __cpp_structured_bindings
 	auto [gcd, valid, x, y] = ::perform_euclidean_algo(num1, num2);
